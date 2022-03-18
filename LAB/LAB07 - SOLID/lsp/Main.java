7723b8c31a0d88bf80f4d1031db5e52ae7c5dc57
62011019 <p.pluemapirak@gmail.com>
2022-03-18 23:41:24 +0700

    edit

--- LAB/LAB07 - SOLID/lsp/Main.java
+++ LAB/LAB07 - SOLID/lsp/Main.java
@@ -6,14 +6,16 @@ public class Main {
     private static final int width = 4;
     private static final int height = 5;
 
-    public static void main(String[] args) {
-        Rectangle reg1 = new Rectangle(width, height);
+    public static void main(String[] args) { //Change Object Rectangle
+       GeometryRectangle reg1 = new Rectangle(width, height);
         System.out.println("==== Rectangle ====");
         System.out.println("Width = " + width);
         System.out.println("Height = " + height);
         System.out.println("Area = " + reg1.getArea());
 
-        Rectangle reg2 = new Square(5);
+        GeometryRectangle reg2 = new Square(5);
+
+        reg2.setSide(20); // set to new side of square
         reg2.setWidth(width);
         reg2.setHeight(height);
         System.out.println("====   Square  ====");
